a2c483ced8da08d090bcbce2b2452769dcffeb5d
Arafeh <Basilarafeh@users.noreply.huggingface.co>
2026-02-16 15:08:58 +0000

    Update frontend/app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = 'https://basilarafeh-documind.hf.space';
 
 const uploadArea = document.getElementById('uploadArea');
 const fileInput = document.getElementById('fileInput');
